9446cdff1773a3976cccae72264a1b4aa1bd7b30
Dylan Sather <dylan.sather@gmail.com>
2016-05-06 15:19:03 -0700

    Adding some echo commands to be verbose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,13 @@
 BUCKET_NAME=dylburger.com
 CONTENT_DIR=/Users/dylan/tree/dylburger.com/content/
 
+echo "Pushing content to ${BUCKET_NAME}"
+
 # Copy files
 aws s3 cp ${CONTENT_DIR}/index.html s3://${BUCKET_NAME}
 aws s3 cp --content-type text/html ${CONTENT_DIR}/burger s3://${BUCKET_NAME}
 aws s3 cp ${CONTENT_DIR}/main.css s3://${BUCKET_NAME}
+
+echo "Done pushing content to ${BUCKET_NAME}"
 
 exit 0
