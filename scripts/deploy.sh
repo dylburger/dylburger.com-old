61b2f883607a5d1d0d3b5d1a58af41157fc016df
Dylan Sather <dylan.sather@gmail.com>
2016-05-06 15:16:45 -0700

    Cleaning up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 BUCKET_NAME=dylburger.com
-
-cd ../content
+CONTENT_DIR=/Users/dylan/tree/dylburger.com/content/
 
 # Copy files
-aws s3 cp index.html s3://${BUCKET_NAME}
-aws s3 cp --content-type text/html burger s3://${BUCKET_NAME}
-aws s3 cp main.css s3://${BUCKET_NAME}
-
-cd -
+aws s3 cp ${CONTENT_DIR}/index.html s3://${BUCKET_NAME}
+aws s3 cp --content-type text/html ${CONTENT_DIR}/burger s3://${BUCKET_NAME}
+aws s3 cp ${CONTENT_DIR}/main.css s3://${BUCKET_NAME}
 
 exit 0
